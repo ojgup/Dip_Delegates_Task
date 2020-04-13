aaef4b65d15b978db94fcd38a0a591456915f680
ojgup <102542177@Student.swin.edu.au>
2020-04-13 11:40:25 +1000

    Added StripWhiteSpace function and start StripQuotes.

--- FileParser/DataParser.cs
+++ FileParser/DataParser.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace FileParser {
     public class DataParser {
@@ -10,8 +11,13 @@ namespace FileParser {
         /// <param name="data"></param>
         /// <returns></returns>
         public List<List<string>> StripWhiteSpace(List<List<string>> data) {
-
-
+            for(int i = 0; i < data.Count; i++)
+            {
+                for(int j = 0; j < data[i].Count; j++)
+                {
+                    data[i][j] = data[i][j].Trim(' ');
+                }
+            }
             return data; //-- return result here
         }
 
@@ -21,9 +27,19 @@ namespace FileParser {
         /// <param name="data"></param>
         /// <returns></returns>
         public List<List<string>> StripQuotes(List<List<string>> data) {
-            
+            string r = "\"test\"";
+
+
+            for(int i = 0; i < data.Count; i++)
+            {
+                for(int j = 0; j < data[i].Count; j++)
+                {
+                    if (data[i][j].StartsWith("\"")){
+                        data[i][j] = data[i][j].TrimStart('"');
+                    }
+                }
+            }
             return data; //-- return result here
         }
-
     }
 }
